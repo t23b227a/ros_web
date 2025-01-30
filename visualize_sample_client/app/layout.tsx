1b09f1072255dc2e189963e3f161bb743311e12b
t23b227a <t23b227a@mail.cc.niigata-u.ac.jp>
2025-01-30 14:46:28 +0900

    ズームイン・ズームアウトの無効化、長押しの無効化

--- visualize_sample_client/app/layout.tsx
+++ visualize_sample_client/app/layout.tsx
@@ -1,16 +1,27 @@
 "use client";
 
+import { useEffect } from "react";
 import { ROSProvider } from './ROSContext';
 import AppNavbar from './components/menubar';
 
 import '@/app/styles/globals.css';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    const disableContextMenu = (event: Event) => event.preventDefault();
+    document.addEventListener("contextmenu", disableContextMenu);
+    document.addEventListener("touchstart", disableContextMenu, { passive: false });
+
+    return () => {
+      document.removeEventListener("contextmenu", disableContextMenu);
+      document.removeEventListener("touchstart", disableContextMenu);
+    };
+  }, []);
   return (
     <html lang="ja">
       <head>
         <title>Ros GUI Controller</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no"></meta>
         <meta name="description" content="GUI Controller for ROS2"></meta>
       </head>
       <body>
