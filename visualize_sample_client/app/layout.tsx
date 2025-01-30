2199b84e685d09422293080803acc0aa297c65c2
t23b227a <t23b227a@mail.cc.niigata-u.ac.jp>
2025-01-30 14:50:06 +0900

    ズームインの無効化、タッチの有効化(修正)

--- visualize_sample_client/app/layout.tsx
+++ visualize_sample_client/app/layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   useEffect(() => {
     const disableContextMenu = (event: Event) => event.preventDefault();
     document.addEventListener("contextmenu", disableContextMenu);
-    document.addEventListener("touchstart", disableContextMenu, { passive: false });
+    // document.addEventListener("touchstart", disableContextMenu, { passive: false });
 
     return () => {
       document.removeEventListener("contextmenu", disableContextMenu);
@@ -21,7 +21,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="ja">
       <head>
         <title>Ros GUI Controller</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no"></meta>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"></meta>
         <meta name="description" content="GUI Controller for ROS2"></meta>
       </head>
       <body>
